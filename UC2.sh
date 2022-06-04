67adacfb929af9a51afb09fbf4b1b392ed555911
Xagar123 <sagarcoc2000@gmail.com>
2022-06-05 02:03:43 +0530

    UC2

---
+++ UC2.sh
@@ -0,0 +1,11 @@
+echo "-----------Welcome to sorting problems-------------"
+#uc1
+read -p "Enter a value: " a
+read -p "Enter b value: " b
+read -p "Enter c value: " c
+
+echo "UC1: Values of a=$a, b=$b and c=$c"
+
+#uc2
+uc2=$((a+b*c))
+echo "UC2: is $uc2"
