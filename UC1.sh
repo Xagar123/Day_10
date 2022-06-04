ceb0f26a611951d482b63489ed4b48968fb1175e
Xagar123 <sagarcoc2000@gmail.com>
2022-06-05 01:56:56 +0530

    UC1

---
+++ UC1.sh
@@ -0,0 +1,7 @@
+echo "-----------Welcome to sorting problems-------------"
+#uc1
+read -p "Enter a value: " a
+read -p "Enter b value: " b
+read -p "Enter c value: " c
+
+echo "UC1: Values of a=$a, b=$b and c=$c"
